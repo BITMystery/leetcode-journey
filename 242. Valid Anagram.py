78427822ad8fdfe4dcd54df0e64a96ea9d4fc501
BITMystery <changliu.bit@gmail.com>
2017-05-06 22:57:53 +0800

    Add files via upload

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,13 @@
+import collections
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return collections.Counter(s) == collections.Counter(t)
+
+s = Solution()
+print s.isAnagram('abba', 'abcb')
