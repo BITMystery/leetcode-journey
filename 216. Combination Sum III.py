03a40b208d479e3608f106974212fe6e9f010267
BITMystery <changliu.bit@gmail.com>
2017-07-20 20:33:41 +1000

    Update 216. Combination Sum III.py

--- 216. Combination Sum III.py
+++ 216. Combination Sum III.py
@@ -1,23 +1,23 @@
 class Solution(object):
-    def Combinations(self, k, n, min_num):
-        if k < 0 or k > 9  or n < (2 * min_num + k - 1) * k / 2 or n > (19 - k) * k / 2:
-            return []
-        if k == 1:
-            return [[n]]
-        res = []
-        for i in xrange(min_num + 1, 10):
-            tmp = self.Combinations(k - 1, n - i + 1, i)
-            for comb in tmp:
-                res.append([i - 1] + comb)
-        return res
+    def dfs(self, start, k, target, path, res):
+        if target < 0:
+            return # bad leaf
+        if target > 0:
+            if len(path) >= k:
+                return # bad leaf
+        if target == 0:
+            if len(path) == k:
+                res.append(path)
+            return
+        for i in xrange(start, 10):
+            self.dfs(i + 1, k, target - i, path + [i], res)
+            
     def combinationSum3(self, k, n):
         """
         :type k: int
         :type n: int
         :rtype: List[List[int]]
         """
-        return self.Combinations(k, n, 1)
-
-s = Solution()
-print s.combinationSum3(3, 7)
-        
+        res = []
+        self.dfs(1, k, n, [], res)
+        return res
