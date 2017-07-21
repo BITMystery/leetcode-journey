371a55e8c3fc33b17afeddce33701b15ff189a98
BITMystery <changliu.bit@gmail.com>
2017-07-21 14:24:03 +1000

    Update 40. Combination Sum II.py

--- 40. Combination Sum II.py
+++ 40. Combination Sum II.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def dfs(self, candidates, target, start, path, res):
-        if target == 0:
-            res.append(path) # good leaf
-            return
         if target < 0:
             return # bad leaf
+        if target == 0:
+            res.append(path) # reach good leaf
+            return
         for i in xrange(start, len(candidates)):
             if i > start and candidates[i] == candidates[i - 1]:
                 continue
+            if candidates[i] > target:
+                break
             self.dfs(candidates, target - candidates[i], i + 1, path + [candidates[i]], res)
             
     def combinationSum2(self, candidates, target):
