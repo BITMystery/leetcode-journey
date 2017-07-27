ba41c72902e61ec095f9da3d747ffd532ea5dc32
BITMystery <changliu.bit@gmail.com>
2017-07-27 13:56:27 +1000

    Update 632. Smallest Range.py

--- 632. Smallest Range.py
+++ 632. Smallest Range.py
@@ -6,19 +6,18 @@ class Solution(object):
         :type nums: List[List[int]]
         :rtype: List[int]
         """
-        common_range = [(nums[i][0], i, 0) for i in xrange(len(nums))] # element in heap: (value, row, offset)
-        heapq.heapify(common_range)
-        left = -1e5
-        right = max([nums[i][0] for i in xrange(len(nums))])
-        res = [left, right]
-        total = sum([len(nums[i]) for i in xrange(len(nums))])
+        window = [(nums[i][0], i, 0) for i in xrange(len(nums))]
+        heapq.heapify(window)
+        smallest = min([nums[i][0] for i in xrange(len(nums))])
+        largest = max([nums[i][0] for i in xrange(len(nums))])
+        res = smallest, largest
         while True:
-            left, row, offset = heapq.heappop(common_range)
-            if right - left < res[1] - res[0]:
-                res = [left, right]
-            if offset + 1 < len(nums[row]):
-                heapq.heappush(common_range, (nums[row][offset + 1], row, offset + 1))
-                right = max(right, nums[row][offset + 1])
-            else:
+            smallest, row, offset = heapq.heappop(window)
+            if offset + 1 == len(nums[row]):
                 break
+            heapq.heappush(window, (nums[row][offset + 1], row, offset + 1))
+            smallest = window[0][0]
+            largest = max(largest, nums[row][offset + 1])
+            if largest - smallest < res[1] - res[0]:
+                res = smallest, largest
         return res
