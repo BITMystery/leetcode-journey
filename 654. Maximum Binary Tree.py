e68d6654e6ee4097a43e254d2d2664664938c13d
BITMystery <changliu.bit@gmail.com>
2017-08-06 18:14:52 +1000

    Add files via upload

---
+++ 654. Maximum Binary Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        max_index = 0
+        for i in xrange(1, len(nums)):
+            if nums[i] > nums[max_index]:
+                max_index = i
+        root = TreeNode(nums[max_index])
+        root.left = self.constructMaximumBinaryTree(nums[:max_index])
+        root.right = self.constructMaximumBinaryTree(nums[max_index + 1:])
+        return root
