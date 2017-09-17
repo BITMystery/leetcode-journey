02fcd5532011da6da8ad3d3aa6afd59dbef287b8
BITMystery <changliu.bit@gmail.com>
2017-09-17 11:11:21 +1000

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution(object):
     def findAnagrams(self, s, p):
         """
@@ -7,18 +9,16 @@ class Solution(object):
         """
         if len(s) < len(p):
             return []
+        tasks = Counter(p)
         res = []
-        to_do = {}
-        for letter in p:
-            to_do[letter] = to_do.get(letter, 0) + 1
         for i in xrange(len(s)):
-            to_do[s[i]] = to_do.get(s[i], 0) - 1
-            if to_do[s[i]] == 0:
-                del to_do[s[i]]
-            if i > len(p) - 1:
-                to_do[s[i - len(p)]] = to_do.get(s[i - len(p)], 0) + 1
-                if to_do[s[i - len(p)]] == 0:
-                    del to_do[s[i - len(p)]]
-            if not to_do:
+            tasks[s[i]] -= 1
+            if tasks[s[i]] == 0:
+                del tasks[s[i]]
+            if i - len(p) >= 0:
+                tasks[s[i - len(p)]] += 1
+                if tasks[s[i - len(p)]] == 0:
+                    del tasks[s[i - len(p)]]
+            if not tasks:
                 res.append(i - len(p) + 1)
         return res
