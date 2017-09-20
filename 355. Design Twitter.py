1a3945121ed09f086d8e00016764ad0482cc85a5
BITMystery <changliu.bit@gmail.com>
2017-09-20 16:28:31 +1000

    Update 355. Design Twitter.py

--- 355. Design Twitter.py
+++ 355. Design Twitter.py
@@ -1,15 +1,14 @@
 import heapq
 
-class Twitter(object):
 
+class Twitter(object):
     def __init__(self):
         """
         Initialize your data structure here.
         """
         self.time = 0
         self.tweets = {}
-        self.follow_index = {}
-        
+        self.follows = {}
 
     def postTweet(self, userId, tweetId):
         """
@@ -26,58 +25,51 @@ class Twitter(object):
 
     def getNewsFeed(self, userId):
         """
-        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        Retrieve the 10 most recent tweet ids in the user's news feed. Each
+        item in the news feed must be posted by users who the user followed or
+        by the user herself. Tweets must be ordered from most recent to least
+        recent.
         :type userId: int
         :rtype: List[int]
         """
-        res = []
-        s = self.follow_index.get(userId, set())
-        s.add(userId)
-        users = list(s)
-        pointers = [len(self.tweets.get(users[i], [])) - 1 for i in xrange(len(users))]
+        users = list(self.follows.get(userId, set()) | set([userId]))
+        pointers = [len(self.tweets.get(u, [])) - 1 for u in users]
         h = []
         heapq.heapify(h)
         for i in xrange(len(users)):
             if pointers[i] >= 0:
                 heapq.heappush(h, self.tweets[users[i]][pointers[i]] + [i])
-        while len(res) < 10 and h:
-            [time, tweet, i] = heapq.heappop(h)
-            res.append(tweet)
-            pointers[i] -= 1
+                pointers[i] -= 1
+        feed = []
+        while h and len(feed) < 10:
+            time, tweet, i = heapq.heappop(h)
+            feed.append(tweet)
             if pointers[i] >= 0:
                 heapq.heappush(h, self.tweets[users[i]][pointers[i]] + [i])
-        return res
-        
+                pointers[i] -= 1
+        return feed
 
     def follow(self, followerId, followeeId):
         """
-        Follower follows a followee. If the operation is invalid, it should be a no-op.
+        Follower follows a followee. If the operation is invalid, it should be
+        a no-op.
         :type followerId: int
         :type followeeId: int
         :rtype: void
         """
-        if followerId in self.follow_index:
-            self.follow_index[followerId].add(followeeId)
+        if followerId in self.follows:
+            self.follows[followerId].add(followeeId)
         else:
-            self.follow_index[followerId] = set([followeeId])
-        
+            self.follows[followerId] = set([followeeId])
 
     def unfollow(self, followerId, followeeId):
         """
-        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
+        Follower unfollows a followee. If the operation is invalid, it should
+        be a no-op.
         :type followerId: int
         :type followeeId: int
         :rtype: void
         """
-        if followerId in self.follow_index:
-            if followeeId in self.follow_index[followerId]:
-                self.follow_index[followerId].remove(followeeId)
-        
-
-
-# Your Twitter object will be instantiated and called as such:
-# obj = Twitter()
-# obj.postTweet(userId,tweetId)
-# param_2 = obj.getNewsFeed(userId)
-# obj.follow(followerId,followeeId)
-# obj.unfollow(followerId,followeeId)
+        if followerId in self.follows:
+            if followeeId in self.follows[followerId]:
+                self.follows[followerId].remove(followeeId)
